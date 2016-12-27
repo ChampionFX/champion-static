9cc828acbe81b8d614af3dec7d4ff545c6e69f9c
binary-hamedanchi <hamedanchi@gmail.com>
2016-12-27 14:21:24 +0800

    Fix stylelint issues

--- build/stylelint.js
+++ build/stylelint.js
@@ -2,11 +2,7 @@ module.exports = {
     all: {
         src: [
             'src/sass/**/*.scss',
-            '!src/sass/external/**/*.scss',
-            '!src/sass/_constants.scss',
-            '!src/sass/mixin.scss',
-            '!src/sass/functions.scss',
-            '!src/sass/reset.scss',
+            '!src/sass/colors.scss',
         ],
     },
 };
