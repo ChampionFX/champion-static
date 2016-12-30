37a072947d46bda8e91d2bfd25d67dbdd9d3d89a
binary-hamedanchi <hamedanchi@gmail.com>
2016-12-30 11:22:47 +0800

    Add language functions

---
+++ src/javascript/common/language.js
@@ -0,0 +1,56 @@
+const Cookies = require('../lib/js-cookie');
+
+const Language = (function () {
+    const all_languages = function() {
+        return {
+            EN   : 'English',
+            DE   : 'Deutsch',
+            ES   : 'Español',
+            FR   : 'Français',
+            ID   : 'Indonesia',
+            IT   : 'Italiano',
+            JA   : '日本語',
+            PL   : 'Polish',
+            PT   : 'Português',
+            RU   : 'Русский',
+            TH   : 'Thai',
+            VI   : 'Tiếng Việt',
+            ZH_CN: '简体中文',
+            ZH_TW: '繁體中文',
+        };
+    };
+
+    const language_from_url = function() {
+        const regex = new RegExp('^(' + Object.keys(all_languages()).join('|') + ')$', 'i');
+        const langs = window.location.href.split('/').slice(3);
+        let lang = '';
+        langs.forEach((l) => { lang = regex.test(l) ? l : lang; });
+        return lang;
+    };
+
+    let current_lang = null;
+    const language = function() {
+        let lang = current_lang;
+        if (!lang) {
+            lang = (language_from_url() || Cookies.get('language') || 'EN').toUpperCase();
+            current_lang = lang;
+        }
+        return lang;
+    };
+
+    const url_for_language = function(lang) {
+        return window.location.href.replace(new RegExp('\/' + language() + '\/', 'i'), '/' + lang.trim().toLowerCase() + '/');
+    };
+
+    return {
+        all_languages   : all_languages,
+        language        : language,
+        url_for_language: url_for_language,
+    };
+})();
+
+module.exports = {
+    getAllLanguages: Language.all_languages,
+    getLanguage    : Language.language,
+    URLForLanguage : Language.url_for_language,
+};
