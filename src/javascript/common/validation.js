c0ecd37d4e67959f71f20044eae0e855f2912046
binary-hamedanchi <hamedanchi@gmail.com>
2017-01-10 18:07:40 +0800

    Validation for virtual account opening

--- src/javascript/common/validation.js
+++ src/javascript/common/validation.js
@@ -2,7 +2,7 @@ const Validation = (function() {
     'use strict';
 
     const forms = {};
-    const error_class  = 'errorfield';
+    const error_class  = 'error-msg';
     const hidden_class = 'hidden';
 
     const events_map = {
@@ -13,7 +13,7 @@ const Validation = (function() {
     const initForm = (form_selector, fields) => {
         const $form = $(`${form_selector}:visible`);
         if ($form.length && Array.isArray(fields) && fields.length) {
-            forms[form_selector] = { fields: fields };
+            forms[form_selector] = { fields: fields, $form: $form };
             fields.forEach((field) => {
                 field.$ = $form.find(field.selector);
                 if (!field.$.length) return;
@@ -21,12 +21,12 @@ const Validation = (function() {
                 if (field.msg_element) {
                     field.$error = $form.find(field.msg_element);
                 } else {
-                    field.$.parent().append($('<p/>', { class: `${error_class} ${hidden_class}` }));
+                    field.$.parent().append($('<div/>', { class: `${error_class} ${hidden_class}` }));
                     field.$error = field.$.parent().find(`.${error_class}`);
                 }
 
                 const event = events_map[field.$.get(0).localName];
-                field.$.on(event, () => {
+                field.$.unbind(event).on(event, () => {
                     checkField(field);
                 });
             });
@@ -36,34 +36,63 @@ const Validation = (function() {
     // ------------------------------
     // ----- Validation Methods -----
     // ------------------------------
-    const validReq = field => field.$.val().length;
+    const validRequired = value => value.length;
 
-    const validEmail = field => /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$/.test(field.$.val());
+    const validEmail = value => /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$/.test(value);
+
+    const validPassword = value => /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+/.test(value);
+
+    const validLength = (value, options) => (
+        (options.min ? value.length >= options.min : true) &&
+        (options.max ? value.length <= options.max : true)
+    );
+
+    const validCompare = (value, options) => value === $(options.to).val();
 
     const validators_map = {
-        req  : { func: validReq,   message: 'This field is required' },
-        email: { func: validEmail, message: 'Invalid email address' },
+        req     : { func: validRequired, message: 'This field is required' },
+        email   : { func: validEmail,    message: 'Invalid email address' },
+        password: { func: validPassword, message: 'Password should have lower and uppercase letters with numbers.' },
+        length  : { func: validLength,   message: 'You should enter [_1] characters.' },
+        compare : { func: validCompare,  message: 'The two passwords that you entered do not match.' },
     };
+
+    const pass_length = { min: 6, max: 25 };
 
     // --------------------
     // ----- Validate -----
     // --------------------
     const checkField = (field) => {
         let all_is_ok = true,
-            validator,
             message;
 
         field.validations.some((valid) => {
+            let type,
+                options = {};
+
             if (typeof valid === 'string') {
-                validator = validators_map[valid].func;
+                type = valid;
+            } else {
+                type    = valid[0];
+                options = valid[1];
             }
-            if (validator) {
-                field.is_ok = validator(field);
-                if (!field.is_ok) {
-                    message = validators_map[valid].message;
-                    all_is_ok = false;
-                    return true;
+
+            if (type === 'password' && !validLength(field.$.val(), pass_length)) {
+                field.is_ok = false;
+                type = 'length';
+                options = pass_length;
+            } else {
+                const validator = validators_map[type].func;
+                field.is_ok = validator(field.$.val(), options);
+            }
+
+            if (!field.is_ok) {
+                message = options.message || validators_map[type].message;
+                if (type === 'length') {
+                    message = message.replace('[_1]', options.min === options.max ? options.min : `${options.min}-${options.max}`);
                 }
+                all_is_ok = false;
+                return true;
             }
             return false;
         });
@@ -88,7 +117,8 @@ const Validation = (function() {
 
     const validate = (form_selector) => {
         const form = forms[form_selector];
-        form.is_ok = form.fields.every(field => checkField(field));
+        form.is_ok = true;
+        form.fields.forEach((field) => { if (!checkField(field)) form.is_ok = false; });
         return form.is_ok;
     };
 
--- src/javascript/pages/new_account/virtual.js
+++ src/javascript/pages/new_account/virtual.js
@@ -1,49 +1,32 @@
 const ChampionSocket       = require('../../common/socket');
 const Client               = require('../../common/client');
 const default_redirect_url = require('../../common/url').default_redirect_url;
+const Validation           = require('../../common/validation');
 
 const ChampionNewVirtualAccount = (function() {
     'use strict';
 
-    const _passwd_regex = /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])/;
-    // const _code_regex = /.{48}/;
+    const form_selector = '#frm_new_account_virtual';
 
-    let _residences = null,
-        _active = false;
+    let _residences = null;
 
-    let _input_code,
-        _input_pass,
-        _input_rpass,
-        _input_country,
-        _submit_btn,
+    let _submit_btn,
         _input_residence;
-
-    let _code_error,
-        _pass_error_short,
-        _pass_error_char,
-        _pass_error_nomatch,
-        _create_acc_error;
 
     const load = () => {
         if (Client.redirect_if_login()) return;
         const container = $('#champion-container');
-        _input_code      = container.find('#verification-code');
-        _input_pass      = container.find('#password');
-        _input_rpass     = container.find('#r-password');
-        _input_country   = container.find('#residence');
         _input_residence = container.find('#residence');
         _submit_btn      = container.find('#btn-submit');
 
-        _code_error         = container.find('#error-code');
-        _pass_error_short   = container.find('#error-pass-short');
-        _pass_error_char    = container.find('#error-pass-char');
-        _pass_error_nomatch = container.find('#error-pass-nomatch');
-        _create_acc_error   = container.find('#error-create-account');
-
-        _input_code.on('input', validateCode);
-        _input_pass.on('input', validatePass);
-        _input_rpass.on('input', validateRpass);
         _submit_btn.on('click', submit);
+
+        Validation.init(form_selector, [
+            { selector: '#verification-code', validations: ['req', ['length', { min: 48, max: 48, message: 'Please submit a valid verification token.' }]] },
+            { selector: '#password',          validations: ['req', 'password'] },
+            { selector: '#r-password',        validations: ['req', ['compare', { to: '#password' }]] },
+            { selector: '#residence',         validations: ['req'] },
+        ]);
 
         if (!_residences) {
             ChampionSocket.send({ residence_list: 1 }, (response) => {
@@ -53,7 +36,6 @@ const ChampionNewVirtualAccount = (function() {
         } else {
             renderResidences();
         }
-        _active = true;
     };
 
     const renderResidences = () => {
@@ -70,78 +52,21 @@ const ChampionNewVirtualAccount = (function() {
     };
 
     const unload = () => {
-        _input_code.off('input', validateCode);
-        _input_pass.off('input', validatePass);
-        _input_rpass.off('input', validateRpass);
         _submit_btn.off('click', submit);
-
-        _code_error.addClass('hidden');
-        _pass_error_short.addClass('hidden');
-        _pass_error_char.addClass('hidden');
-        _pass_error_nomatch.addClass('hidden');
-        _create_acc_error.addClass('hidden');
-
-        _input_code.val('');
-        _input_pass.val('');
-        _input_rpass.val('');
-        _input_country.val('');
-        _input_residence.empty();
-        _active = false;
-    };
-
-    const validateCode = () => {
-        const value = _input_code.val();
-
-        _create_acc_error.addClass('hidden');
-        _code_error.addClass('hidden');
-        if (value.length < 48) {
-            _code_error.removeClass('hidden');
-            return false;
-        }
-        return true;
-    };
-
-    const validatePass = () => {
-        const value = _input_pass.val();
-
-        _create_acc_error.addClass('hidden');
-        _pass_error_short.addClass('hidden');
-        _pass_error_char.addClass('hidden');
-
-        if (value.length < 6) {
-            _pass_error_short.removeClass('hidden');
-            return false;
-        } else if (!_passwd_regex.test(value)) {
-            _pass_error_char.removeClass('hidden');
-            return false;
-        }
-
-        validateRpass();
-
-        return true;
-    };
-
-    const validateRpass = () => {
-        _pass_error_nomatch.addClass('hidden');
-        if (_input_pass.val() !== _input_rpass.val()) {
-            _pass_error_nomatch.removeClass('hidden');
-            return false;
-        }
-        return true;
     };
 
     const submit = (e) => {
         e.preventDefault();
-        if (_active && validateCode() && validatePass()) {
+        if (Validation.validate(form_selector)) {
             const data = {
                 new_account_virtual: 1,
-                verification_code  : _input_code.val(),
-                client_password    : _input_pass.val(),
-                residence          : _input_residence.val(),
+                verification_code  : $('#verification-code').val(),
+                client_password    : $('#password').val(),
+                residence          : $('#residence').val(),
             };
             ChampionSocket.send(data, (response) => {
                 if (response.error) {
-                    _create_acc_error.removeClass('hidden').text(response.error.message);
+                    $('#error-create-account').removeClass('hidden').text(response.error.message);
                 } else {
                     const acc_info = response.new_account_virtual;
                     Client.process_new_account(acc_info.email, acc_info.client_id, acc_info.oauth_token, true);
