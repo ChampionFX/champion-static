d8bb6b5adf39c1215b7990198fe176c3640623d2
binary-hamedanchi <hamedanchi@gmail.com>
2017-01-11 10:17:23 +0800

    Fix release --cleanup

--- build/gh-pages.js
+++ build/gh-pages.js
@@ -1,6 +1,6 @@
 module.exports = function (grunt) {
     var is_cleanup   = !!grunt.option('cleanup'),
-        cleanup_only = global.branch ? [global.branch_prefix + global.branch + '/.'] : (global.release_target ? ['.', '!beta/.', '!translations/.'] : ['.']),
+        cleanup_only = global.branch ? [global.branch_prefix + global.branch + '/**'] : (global.release_target ? ['**/*', '!beta/**', '!translations/**'] : ['**/*']),
         source       = global.branch ? [global.branch_prefix + global.branch + '/**'] : ['**/*', '!' + (global.branch_prefix || 'br_') + '*/**', '!translations/**', '!beta/**'];
 
     return {
