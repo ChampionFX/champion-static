f5883cc7768ec1f88e4b1a4a02f195bd0e65377f
kelly <kelly@binary.com>
2017-01-17 13:54:16 +0800

    Change url to user/settings

--- scripts/config/pages.pl
+++ scripts/config/pages.pl
@@ -30,7 +30,7 @@ sub all_pages {
         ['new-account/virtual',        'new_account/virtual',          'full_width', 'Create new virtual account'],
         ['new-account/real',           'new_account/real',             'full_width', 'Create new real account'],
 
-        ['settings',                   'user/settings',                'full_width', 'Settings'],
+        ['user/settings',              'user/settings',                'full_width', 'Settings'],
         ['user/change-password',       'user/change_password',         'full-width', 'Change Password'],
 
     );
