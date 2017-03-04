731e374dbf0340fd2e5704da937b52294ba5cb7e
nazaninreihani <nazanin.reyh@gmail.com>
2017-03-04 15:44:15 +0330

    add slider - change menu

--- src/javascript/pages/slider.js
+++ src/javascript/pages/slider.js
@@ -14,4 +14,18 @@ const Slider = (() => {
     };
 })();
 
+const positionFooterAndDots = function() {
+    const height = $('.fx-slider-footer').innerHeight();
+    setTimeout(function () {
+        $('.fx-slider-footer').css('bottom', height);
+        $('.slick-dots').css('bottom', height + 40);
+        $('.fx-slider-text').css('margin-bottom', height);
+    }, 10);
+};
+
+$(document).ready(
+    function() {
+        positionFooterAndDots();
+        $(window).resize(positionFooterAndDots);
+    });
 module.exports = Slider;
