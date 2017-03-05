3222e7ebc1aea3b50ad2e066f083b26c67aeb04e
nazaninreihani <nazanin.reyh@gmail.com>
2017-03-06 01:03:52 +0330

    slider fix

--- src/javascript/pages/slider.js
+++ src/javascript/pages/slider.js
@@ -20,21 +20,28 @@ const Slider = (() => {
 const positionFooterAndDots = function() {
     const height = -$('.slider-footer').innerHeight();
     const dotsMargin = height - 40;
-    if (window.matchMedia('(min-width: 796px)').matches) {
+    if (window.matchMedia('(min-width: 797px)').matches) {
         /*eslint-disable */
-        $('.slider-footer').css({
-            transform: 'translateY(' + height + 'px)',
-        });
-        $('#slider-dots').css({
-            transform: 'translateY(' + dotsMargin + 'px)',
-        });
-        $('.slider-text').css('margin-bottom', -height + 'px');
+        setTimeout(function () {
+          $('.slider-footer').css({
+              transform: 'translateY(' + height + 'px)',
+          });
+          $('#slider-dots').css({
+              transform: 'translateY(' + dotsMargin + 'px)',
+          });
+          $('.slider-text').css('margin-bottom', -height + 'px');
+        }, 10);
+
         /*eslint-enable */
     } else {
         setTimeout(function () {
-            $('.slick-dots').css({
+            $('.slider-footer').css({
+                transform: 'translateY(0)',
+            });
+            $('#slider-dots').css({
                 transform: 'translateY(-40px)',
             });
+            $('.slider-text').css('margin-bottom', 0);
         });
     }
 };
