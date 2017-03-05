b67335c3e56b420344be99209fde72e32145f072
nazaninreihani <nazanin.reyh@gmail.com>
2017-03-05 16:19:02 +0330

    menu design - sign up hide on login

--- src/javascript/pages/slider.js
+++ src/javascript/pages/slider.js
@@ -17,17 +17,25 @@ const Slider = (() => {
 const positionFooterAndDots = function() {
     const height = -$('.slider-footer').innerHeight();
     const dotsMargin = height - 40;
-    setTimeout(function () {
+    if (window.matchMedia('(min-width: 796px)').matches) {
+        setTimeout(function () {
         /*eslint-disable */
-        $('.slider-footer').css({
-            transform: 'translateY(' + height + 'px)',
-        });
-        $('.slick-dots').css({
-            transform: 'translateY(' + dotsMargin + 'px)',
-        });
-        $('.slider-text').css('margin-bottom', -height + 'px');
+            $('.slider-footer').css({
+                transform: 'translateY(' + height + 'px)',
+            });
+            $('.slick-dots').css({
+                transform: 'translateY(' + dotsMargin + 'px)',
+            });
+            $('.slider-text').css('margin-bottom', -height + 'px');
         /*eslint-enable */
-    }, 10);
+        }, 10);
+    } else {
+        setTimeout(function () {
+            $('.slick-dots').css({
+                transform: 'translateY(-40px)',
+            });
+        });
+    }
 };
 
 $(document).ready(
