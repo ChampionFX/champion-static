47589088cd806fc408d707e0a6ddd099a8ad3ba4
kelly <kelly@binary.com>
2017-06-14 14:45:17 +0800

    Add scroll tab

--- src/javascript/pages/cashier/payment_methods.js
+++ src/javascript/pages/cashier/payment_methods.js
@@ -1,6 +1,9 @@
 const ChampionSocket = require('./../../common/socket');
 const Client         = require('../../common/client');
 
+require('../../components/scrollTabs/jquery.scrolltabs');
+require('../../components/scrollTabs/jquery.mousewheel');
+
 const CashierPaymentMethods = (function() {
     'use strict';
 
@@ -8,12 +11,32 @@ const CashierPaymentMethods = (function() {
 
     const load = () => {
         ChampionSocket.wait('authorize').then(() => {
-            $('.has-tabs').tabs().removeClass('invisible');
+            $('.tab-content').addClass('in')
             $('#accordion').accordion({
                 heightStyle : 'content',
                 collapsible : true,
                 active      : false
             });
+            $('.hasTabs').scrollTabs({
+                scroll_distance: 350,
+                scroll_duration: 350,
+                left_arrow_size: 40,
+                right_arrow_size: 40,
+                click_callback: function(e){
+                    var val = $(this).find('a').attr('rel');
+                    $(this).parent().parent().find('.scroll_tab_active').removeClass('scroll_tab_active');
+                    $(this).addClass('scroll_tab_active');
+                    console.log($(this).parent().parent());
+                    if (val) {
+                        console.log(val);
+                        $('.tab-content').find('.tab-content-wrapper > div').addClass('invisible');
+                        $(val).removeClass('invisible').addClass('slide-in');
+                    }
+                }
+            });
+            // $('.slick-tab-content').slick();
+            // $('.has-tabs').tabs().removeClass('invisible');
+
             const container = $('.fx-payment-methods');
             if (!Client.is_logged_in()) {
                 container.find('#btn-open-account').removeClass(hidden_class);
