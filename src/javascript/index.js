319bc847ac0e4e35c9f2be812d2c271a65a066b2
kelly <kelly@binary.com>
2017-06-19 18:07:18 +0800

    Change tab height to 65px

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -8,3 +8,13 @@ require('./pages/contact');
 const Champion = require('./common/champion');
 
 $(window).on('load', Champion.init);
+
+$.fn.scrollEnd = function(callback, timeout) {
+    $(this).scroll(function(e) {
+        const $this = $(this);
+        if ($this.data('scrollTimeout')) {
+            clearTimeout($this.data('scrollTimeout'));
+        }
+        $this.data('scrollTimeout', setTimeout(() => callback.call(this, e), timeout));
+    });
+};
--- src/javascript/pages/cashier/payment_methods.js
+++ src/javascript/pages/cashier/payment_methods.js
@@ -3,8 +3,11 @@ const ChampionSocket = require('./../../common/socket');
 const CashierPaymentMethods = (function() {
     'use strict';
 
-    const hidden_class = 'invisible';
-    let mobile = $(window).innerWidth() < 767;
+    const SCROLL_STEP = 5,
+        hidden_class = 'invisible';
+
+    let isVertical = $(window).innerWidth() < 767,
+        scrolledTabs = 1; // scrolledTabs = tab number
 
     const load = () => {
         ChampionSocket.wait('authorize').then(() => {
@@ -20,100 +23,108 @@ const CashierPaymentMethods = (function() {
             });
 
             $(window).on('orientationchange resize', () => {
-                mobile = $(window).innerWidth() < 767;
+                isVertical = $(window).innerWidth() < 767;
             });
 
             $('#payment_methods').removeClass(hidden_class);
-            scrollContentHandler();
-            clickToScrollHandler();
-            swipeToScrollHandler();
+            scrollTabContents();
+            $('.scroll-right-button').unbind('click').click(scrollHandler(true));
+            $('.scroll-left-button').unbind('click').click(scrollHandler(false));
+            $('.scrollable-tabs').scrollEnd(toggleNextAndPrevious, 50);
         });
     };
 
-    const scrollContentHandler = () => {
+    const scrollTabContents = () => {
+        const $tab_content = $('.tab-content-wrapper');
         $('.scrollable-tabs li').click(function(e) {
             e.preventDefault();
             const val = $(this).find('a').attr('rel');
             $(this).parent().find('.tab-selected').removeClass('tab-selected');
             $(this).addClass('tab-selected');
-            if (val && mobile) {
-                $('.tab-content-wrapper').animate({ scrollTop: $('.tab-content-wrapper').scrollTop() + $(val).position().top }, 350);
+            if (val && isVertical) {
+                $tab_content.animate({ scrollTop: $tab_content.scrollTop() + $(val).position().top }, 350);
             } else {
-                $('.tab-content-wrapper').animate({ scrollLeft: $('.tab-content-wrapper').scrollLeft() + $(val).position().left }, 500);
+                $tab_content.animate({ scrollLeft: $tab_content.scrollLeft() + $(val).position().left }, 500);
             }
         });
     };
 
-    const clickToScrollHandler = () => {
-        let n = 1; // n = tab number
-        const num_of_tabs = $('.scrollable-tabs').children().length;
-        $('.scroll-right-button').unbind('click').click(function(e) {
-            e.preventDefault();
-            if (num_of_tabs > 5) {
-                n += 5;
-            } else {
-                n += num_of_tabs;
-            }
-            if (n < num_of_tabs) {
-                $('.scroll-left-button').removeClass(hidden_class);
-                $(this).siblings('.col-md-11').removeClass('col-md-11').addClass('col-md-10');
-                if (mobile) {
-                    $('.scrollable-tabs').animate({ scrollTop: $('.scrollable-tabs').scrollTop() + $(`.scrollable-tabs :nth-child(${n})`).position().top }, 500);
-                } else {
-                    $('.scrollable-tabs').animate({ scrollLeft: $('.scrollable-tabs').scrollLeft() + $(`.scrollable-tabs :nth-child(${n})`).position().left }, 500);
-                }
-            }
-            swipeToScrollHandler();
-        });
-        $('.scroll-left-button').unbind('click').click(function(e) {
-            e.preventDefault();
-            if (num_of_tabs > 5) {
-                n -= 5;
-            } else {
-                n -= num_of_tabs;
-            }
-            n = n < 0 ? 1 : n;
-            if (n < num_of_tabs) {
-                $('.scroll-left-button').removeClass(hidden_class);
-                $(this).siblings('.col-md-11').removeClass('col-md-11').addClass('col-md-10');
-                if (mobile) {
-                    $('.scrollable-tabs').animate({ scrollTop: $('.scrollable-tabs').scrollTop() + $(`.scrollable-tabs :nth-child(${n})`).position().top }, 500);
-                } else {
-                    $('.scrollable-tabs').animate({ scrollLeft: $('.scrollable-tabs').scrollLeft() + $(`.scrollable-tabs :nth-child(${n})`).position().left }, 500);
-                }
-            }
-        });
+    const scrollHandler = isNextButton => (e) => {
+        e.preventDefault();
+        scrolledTabs = incrementScrolledTabs(isNextButton);
+        if (isThereChildrenToScroll()) {
+            scroll(isVertical);
+        }
     };
 
-    const swipeToScrollHandler = () => {
-        const height = Math.ceil($('.scrollable-tabs').height());
-        const width = Math.ceil($('.scrollable-tabs').width());
-        $('.scrollable-tabs').scroll(function() {
-            if (mobile) {
-                if ($('.scrollable-tabs :nth-child(1)').position().top === 0) {
-                    hideButton($('.scroll-left-button'));
-                } else if ($(this).get(0).scrollHeight - $(this).scrollTop() === height) {
-                    hideButton($('.scroll-right-button'));
-                } else {
-                    $('.scroll-left-button').removeClass(hidden_class);
-                    $('.scroll-right-button').removeClass(hidden_class);
-                    $('.scrollable-tabs').addClass('in-the-middle');
-                }
-                return;
+    function toggleNextAndPrevious () {
+        if (isVertical) {
+            const height = Math.ceil($('.scrollable-tabs').height());
+
+            if ($('.scrollable-tabs :nth-child(1)').position().top === 0) {
+                hideButton($('.scroll-left-button'));
+            } else if ($(this).get(0).scrollHeight - $(this).scrollTop() === height) {
+                hideButton($('.scroll-right-button'));
+            } else {
+                showBothButtons();
+                makeScrollTabsSmall(true);
             }
+        } else {
+            const width = Math.ceil($('.scrollable-tabs').width());
             if ($('.scrollable-tabs :nth-child(1)').position().left === 0) {
                 hideButton($('.scroll-left-button'));
-            } else if ($(this).get(0).scrollWidth - $(this).scrollLeft() === width) {
+            } else if (Math.abs($(this).get(0).scrollWidth - $(this).scrollLeft() - width) < 5) {
                 hideButton($('.scroll-right-button'));
+            } else {
+                showBothButtons();
+                makeScrollTabsSmall(false);
             }
-        });
-    };
+        }
+    }
 
     const hideButton = (element) => {
         element.siblings('div.col-md-10').removeClass('col-md-10').addClass('col-md-11');
         element.siblings().removeClass(hidden_class);
         element.addClass(hidden_class);
         $('.scrollable-tabs').removeClass('in-the-middle');
+    };
+
+    const incrementScrolledTabs = (isDirectionForward = true) => {
+        const num_of_tabs = $('.scrollable-tabs').children().length;
+
+        if (num_of_tabs > 5) {
+            return isDirectionForward ? scrolledTabs + SCROLL_STEP : scrolledTabs - SCROLL_STEP;
+        }
+
+        return isDirectionForward ? scrolledTabs + num_of_tabs : scrolledTabs - num_of_tabs;
+    };
+
+    const isThereChildrenToScroll = () => {
+        const num_of_tabs = $('.scrollable-tabs').children().length;
+        return scrolledTabs < num_of_tabs && scrolledTabs > 0;
+    };
+
+    const scroll = () => {
+        if (isThereChildrenToScroll()) {
+            if (isVertical) {
+                $('.scrollable-tabs').animate({ scrollTop: $('.scrollable-tabs').scrollTop() + $(`.scrollable-tabs :nth-child(${scrolledTabs})`).position().top }, 500);
+            } else {
+                $('.scrollable-tabs').animate({ scrollLeft: $('.scrollable-tabs').scrollLeft() + $(`.scrollable-tabs :nth-child(${scrolledTabs})`).position().left }, 500);
+            }
+        }
+    };
+
+    const showBothButtons = () => {
+        $('.scroll-left-button').removeClass(hidden_class);
+        $('.scroll-right-button').removeClass(hidden_class);
+    };
+
+    const makeScrollTabsSmall = () => {
+        if (isVertical) {
+            $('.scrollable-tabs').addClass('in-the-middle');
+        } else {
+            $('.scrollable-tabs').parent().removeClass('col-md-11').addClass('col-md-10');
+        }
     };
 
     return {
