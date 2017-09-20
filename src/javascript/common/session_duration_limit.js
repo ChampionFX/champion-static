02a1cb932aa7bb5b4df0f850c67c557a4c16493c
binary-hamedanchi <hamedanchi@gmail.com>
2017-09-20 15:13:13 +0800

    Improve lightbox

--- src/javascript/common/session_duration_limit.js
+++ src/javascript/common/session_duration_limit.js
@@ -56,7 +56,8 @@ const SessionDurationLimit = (() => {
 
     const displayWarning = () => {
         $('body').append(
-            $(`<div id='session_limit' class='lightbox'><div><div><div class='limit_message'>${template('Your session duration limit will end in [_1] seconds.', [warning / 1000])}</div></div></div></div>`));
+            $('<div/>', { id: 'session_limit', class: 'lightbox' })
+                .append($('<div/>', { class: 'limit_message', text: template('Your session duration limit will end in [_1] seconds.', [warning / 1000]) })));
         $('#session_limit').click(function() { $(this).remove(); });
     };
 
