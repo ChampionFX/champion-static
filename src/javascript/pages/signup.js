e84783ae5784fe9e7da3d254b28ac71f674e4f25
kelly <kelly@binary.com>
2017-09-27 10:40:12 +0800

    Update signup modal js

--- src/javascript/pages/signup.js
+++ src/javascript/pages/signup.js
@@ -15,6 +15,21 @@ const ChampionSignup = (function() {
         $button;
 
     const load = () => {
+        $('.toggle-modal').off('click').on('click', function(e) {
+            e.stopPropagation();
+            $('.modal').toggleClass('modal--show');
+            if ($('.modal--show').length) {
+                $('body').css('position', 'static').append('<div class="modal-overlay"></div>');
+                $('html').css('overflow-y', 'hidden');
+            }
+        });
+        $('.modal__header .close').off('click').on('click', function(e) {
+            e.stopPropagation();
+            $('.modal').removeClass('modal--show');
+            $('.modal-overlay').remove();
+            $('html').css('overflow-y', 'auto');
+        });
+
         if (Client.is_logged_in() || /(new-account|terms-and-conditions|user|cashier)/.test(window.location.pathname)) {
             changeVisibility($(form_selector), 'hide');
         } else {
@@ -43,7 +58,7 @@ const ChampionSignup = (function() {
         $button.off('click', submit).on('click', submit);
         is_active = true;
         Validation.init(form_selector, [
-            { selector: '#email', validations: ['req', 'email'], msg_element: '#signup_error' },
+            { selector: '#email', validations: ['req', 'email'], msg_element: '#signup_error', no_scroll: true },
         ]);
     };
 
@@ -53,6 +68,8 @@ const ChampionSignup = (function() {
             $input.val('');
         }
         is_active = false;
+        $('toggle-notification').off('click');
+        $('.modal__header .close').off('click');
     };
 
     const submit = (e) => {
@@ -63,7 +80,8 @@ const ChampionSignup = (function() {
                 type        : 'account_opening',
             }).then((response) => {
                 if (response.verify_email) {
-                    $('.signup-box').text('Thank you for signing up! Please check your email to complete the registration process.');
+                    $('.modal__form_message').removeClass('invisible');
+                    $('.modal__form_wrapper, .modal__body').addClass('invisible');
                 } else if (response.error) {
                     $(`${form_selector}:visible #signup_error`).text(response.error.message).removeClass(hidden_class);
                 }
